dd09353add36ec12a164c53592dc05a435ebd4b8
Leon Patmore <leon.patmore@bath.edu>
2022-09-01 19:52:39 +0100

    fixing windows.

--- install-binary.sh
+++ install-binary.sh
@@ -3,5 +3,4 @@ version="$(cat plugin.yaml | grep "version" | cut -d ' ' -f 2)"
 
 echo "Installing version $version"
 
-go build -o $HELM_PLUGIN_DIR/bin/helm-plugin
-chmod +x $HELM_PLUGIN_DIR/bin/helm-plugin
+make build
