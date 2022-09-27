c8c0983976ace480f41c165daed7c9eea8723490
Leon Patmore <leon.patmore@bath.edu>
2022-09-27 19:16:18 +0100

    adding test for get secret.

--- config.go
+++ config.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"os"
+
 	"github.com/aws/aws-secretsmanager-caching-go/secretcache"
 )
 
 var secretCache, _ = secretcache.New()
 
 func getSecret(id string) (string, error) {
+	env := os.Getenv(id)
+	if len(env) > 0 {
+		return env, nil
+	}
 	res, err := secretCache.GetSecretString(id)
 	return res, err
 }
--- config_test.go
+++ config_test.go
@@ -1,12 +1,24 @@
 package main
 
 import (
+	"os"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestGetSecret(t *testing.T) {
 	t.Skip("Only used for e2e testing")
     _, err := getSecret("example")
+    if err != nil {
+        t.Fatal(err)
+    }
+}
+
+func TestGetSecret_usesEnvVar(t *testing.T) {
+    os.Setenv("SOME_EXAMPLE", "val")
+    val, err := getSecret("SOME_EXAMPLE")
+    assert.Equal(t, "val", val)
     if err != nil {
         t.Fatal(err)
     }
