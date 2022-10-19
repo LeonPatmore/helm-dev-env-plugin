226f9d80a915237a7ea53fecf0a4bf6c1688a440
Leon Patmore <leon.patmore@bath.edu>
2022-10-19 22:14:07 +0100

    mocking.

--- deployment_values.go
+++ deployment_values.go
@@ -9,22 +9,28 @@ import (
 var org, err = getSecret("GITHUB_ORG")
 
 type Configuration interface {
-	GetContents(ctx context.Context, owner, repo, path string, opts string) github.Client
+	GetContents(ctx context.Context, owner string, repo string, path string, opts *github.RepositoryContentGetOptions) (string, error)
 }
 
-type GithubConfiguration struct
-
-func init() {
-	if err != nil {
-		panic("Error while trying to get the gtihub org")
-	}
+type GithubConfiguration struct {
+	client github.Client
 }
 
-func getDeploymentValuesFromRepo(repo string, client github.Client) (string, error) {
-	ctx := context.Background()
-	fileContent, _, _, err := client.Repositories.GetContents(ctx, org, repo, "deployment.yaml", &github.RepositoryContentGetOptions{Ref: "OTT-XXX/Fix-Values"})
+func (r GithubConfiguration) GetContents(ctx context.Context, owner string, repo string, path string, opts *github.RepositoryContentGetOptions) (string, error) {
+	fileContent, _, _, err := r.client.Repositories.GetContents(ctx, owner, repo, path, opts)
 	if err != nil {
 		return "", err
 	}
 	return fileContent.GetDownloadURL(), nil
+}
+
+func init() {
+	if err != nil {
+		panic("Error while trying to get the github org")
+	}
+}
+
+func getDeploymentValuesFromRepo(repo string, configuration Configuration) (string, error) {
+	ctx := context.Background()
+	return configuration.GetContents(ctx, org, repo, "deployment.yaml", &github.RepositoryContentGetOptions{Ref: "OTT-XXX/Fix-Values"})
 }
--- deployment_values_test.go
+++ deployment_values_test.go
@@ -1,9 +1,18 @@
 package main
 
 import (
+	"context"
 	"testing"
+
+	"github.com/google/go-github/v47/github"
 )
+
+type MockConfiguration struct {}
+
+func (r MockConfiguration) GetContents(ctx context.Context, owner string, repo string, path string, opts *github.RepositoryContentGetOptions) (string, error) {
+	return "a",  nil
+}
 
 func TestGetDeploymentValuesFromRepo(t *testing.T) {
 	getDeploymentValuesFromRepo("a", nil)
-}
+}	
